f7224dcd5e33ee1819c084b9cf17cc1499c51da6
ydm2011 <ydm2011@163.com>
2014-09-24 16:31:34 +0800

    fix the test unitest main.cpp!

--- main.cpp
+++ main.cpp
@@ -21,37 +21,14 @@ int main()
     in.read(&contents[0],contents.size());
     in.close();
 
-    ExtractBySunday test_extract;
-    list<size_t> match_locations;
-    size_t match_postion;
-    time_t start;
-    test_extract.getMatch(contents.c_str(),
-                          contents.size(),
-                          "<ul ",4,match_locations,
-                          ExtractContentInterface::ALL);
-    test_extract.extractTag(contents.c_str(),contents.size(),
-                            "<ul ",4,
-                            match_postion);
-    cout<<string(contents.c_str(),match_locations.front(),4)<<endl;
-    size_t attribute_loca;
-    test_extract.getAttributeOfTag(contents.c_str()+match_postion,contents.size()-match_postion,"id",attribute_loca);
-    string attribute_value;
-    test_extract.extract(contents.c_str()+match_postion+attribute_loca,contents.size()-match_postion,"id",2,attribute_value);
-    cout<<"id:"<<attribute_value<<endl;
+
 
     ExtractBySunday *extract = new ExtractBySunday;
     ExtractUrlFromSo extract_so(extract);
-    pair<string,string> id("id","m-result");
-    cout<<"content size:"<<contents.size()<<endl;
-    size_t result_loca;
-
-    extract_so.getSearchResultContainer(contents.c_str(),contents.size(),id,result_loca,"<ul ");
 
     list<string> urls;
     extract_so.getUrls(contents.c_str(),contents.size(),"<h3",urls);
-    string temp_test(contents,result_loca,10);
-    cout<<temp_test<<endl;
-    cout << "Hello World!" << endl;
+
     return 0;
 }
 
